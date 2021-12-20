6a887c95a929fe658b5940b7b1cb6ab2b21f5343
Emi <emi.veisllari@gmail.com>
2021-12-20 14:54:10 +0100

    Update fixes on index.js

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-const projects = [{
-    title: 'To do list app',
-    image: 'images/Screenshot24.png',
-    description: "A simple to do list Application which dynamically renders tasks in JavaScript using the Webpack. Projects and tasks can be created and stored in Web browser's local storage.",
-    lang1: 'html',
-    lang2: 'css',
-    lang3: 'JavaScript',
-    lang4: 'Bootstrap',
-    lang5: 'gpages',
-    lang6: 'Terminal',
-    lang7: 'Webpack',
-    lang8: 'Github',
-  }];
-  const [project] = projects;
-  const button = document.querySelectorAll('.popup');
-  const popup = document.querySelector('.popup-window');
-  
-  popup.innerHTML = `<div class="container">
+const projects = [
+  {
+    title: "To do list app",
+    image: "images/Screenshot24.png",
+    description:
+      "A simple to do list Application which dynamically renders tasks in JavaScript using the Webpack. Projects and tasks can be created and stored in Web browser's local storage.",
+    lang1: "html",
+    lang2: "css",
+    lang3: "JavaScript",
+    lang4: "Bootstrap",
+    lang5: "gpages",
+    lang6: "Terminal",
+    lang7: "Webpack",
+    lang8: "Github",
+  },
+];
+const [project] = projects;
+const button = document.querySelectorAll(".popup");
+const popup = document.querySelector(".popup-window");
+
+popup.innerHTML = `<div class="container">
   <button class="close-button"><i class="fa fa-times"></i></button>
   <img src="${project.image}" alt="project-img" class="popup-img">
   <div class="header-container">
@@ -43,29 +46,28 @@ const projects = [{
   </div>
   
   </div>`;
-  const close = document.querySelector('.close-button');
-  close.addEventListener('click', () => {
-    popup.classList.replace('window', 'invisible');
-    document.body.classList.remove('isfixed');
-  });
-  
+const close = document.querySelector(".close-button");
+close.addEventListener("click", () => {
+  popup.classList.replace("window", "invisible");
+  document.body.classList.remove("isfixed");
+});
+
 //   button.forEach((but) => {
 //     but.addEventListener('click', () => {
 //       document.body.classList.add('isfixed');
 //       popup.classList.replace('invisible', 'window');
 //     });
 //   });
-  
-  const hamburger = document.querySelector('.fa-bars');
-  const menu = document.querySelector('#menu');
-  const menuLis = menu.querySelectorAll('li');
-  
-  hamburger.addEventListener('click', () => {
-    menu.classList.replace('invisible', 'menu');
+
+const hamburger = document.querySelector(".fa-bars");
+const menu = document.querySelector("#menu");
+const menuList = menu.querySelectorAll("li");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.replace("invisible", "menu");
+});
+for (let i = 0; i < menuList.length; i += 1) {
+  menuList[i].addEventListener("click", () => {
+    menu.classList.replace("menu", "invisible");
   });
-  for (let i = 0; i < menuLis.length; i += 1) {
-    menuLis[i].addEventListener('click', () => {
-      menu.classList.replace('menu', 'invisible');
-    });
-  }
-  
+}
