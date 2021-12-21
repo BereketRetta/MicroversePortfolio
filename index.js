9347796795595065997c7cac3b9482ddd17a01fd
Bereket Retta <bereket@etmsoftwareplc.com>
2021-12-21 16:33:43 +0300

    FIX SOME LINTER ISSUE

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const projects = [{
-  title: 'To do list app',
-  image: 'images/Screenshot24.png',
-  description: "A simple to do list Application which dynamically renders tasks in JavaScript using the Webpack. Projects and tasks can be created and stored in Web browser's local storage.",
-  lang1: 'html',
-  lang2: 'css',
-  lang3: 'JavaScript',
-  lang4: 'Bootstrap',
-  lang5: 'gpages',
-  lang6: 'Terminal',
-  lang7: 'Webpack',
-  lang8: 'Github',
+    title: 'To do list app',
+    image: 'images/Screenshot24.png',
+    description:
+      'A simple to do list Application which dynamically renders tasks in JavaScript using the Webpack. Projects and tasks can be created and stored in Web browser"s local storage.',
+    lang1: 'html',
+    lang2: 'css',
+    lang3: 'JavaScript',
+    lang4: 'Bootstrap',
+    lang5: 'gpages',
+    lang6: 'Terminal',
+    lang7: 'Webpack',
+    lang8: 'Github',
 }];
+
 const [project] = projects;
 const button = document.querySelectorAll(".popup");
 const popup = document.querySelector(".popup-window");
-
 
 popup.innerHTML = `<div class="container">
 <button class="close-button"><i class="fa fa-times"></i></button>
@@ -42,34 +43,31 @@ popup.innerHTML = `<div class="container">
 <button><a href="#" target="_blank">See Live</a><span><img src="./images/Icon-Export.png" alt="github"></span></button>
 <button><a href="#" target="_blank">See Source</a><span><img src="./images/Git.png" alt="github"></span></button>
 </div>
-
 </div>`;
-const close = document.querySelector('.close-button');
-close.addEventListener('click', () => {
-  popup.classList.replace('window', 'invisible');
-  document.body.classList.remove('isfixed');
+
+const close = document.querySelector(".close-button");
+close.addEventListener("click", () => {
+  popup.classList.replace("window", "invisible");
+  document.body.classList.remove("isfixed");
 });
 
 button.forEach((but) => {
-  but.addEventListener('click', () => {
-    document.body.classList.add('isfixed');
-    popup.classList.replace('invisible', 'window');
+  but.addEventListener("click", () => {
+    document.body.classList.add("isfixed");
+    popup.classList.replace("invisible", "window");
   });
 });
 
-const hamburger = document.querySelector('.fa-bars');
-const menu = document.querySelector('#menu');
-const menuLis = menu.querySelectorAll('li');
+const hamburger = document.querySelector(".fa-bars");
+const menu = document.querySelector("#menu");
+const menuLis = menu.querySelectorAll("li");
 
-hamburger.addEventListener('click', () => {
-  menu.classList.replace('invisible', 'menu');
+hamburger.addEventListener("click", () => {
+  menu.classList.replace("invisible", "menu");
 });
+
 for (let i = 0; i < menuLis.length; i += 1) {
-  menuLis[i].addEventListener('click', () => {
-    menu.classList.replace('menu', 'invisible');
+  menuLis[i].addEventListener("click", () => {
+    menu.classList.replace("menu", "invisible");
   });
-}
-
-function isLower(str) {
-  return !(/[a-z]/.test(str) && !/[A-Z]/.test(str));
 }
